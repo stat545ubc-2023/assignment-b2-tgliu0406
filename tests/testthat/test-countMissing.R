0877690da5a59cc23a5630cf76c0879a13ffd91e
tgliu0406 <tiangeliu0528@gmail.com>
2023-11-04 15:04:19 -0700

    Update version, correct typos in test file

--- tests/testthat/test-countMissing.R
+++ tests/testthat/test-countMissing.R
@@ -1,18 +1,18 @@
 test_that("Output matches direct call to dplyr", {
-  small_tbl <- tribble(~group, ~var1, ~var2,
+  small_tbl <- dplyr::tribble(~group, ~var1, ~var2,
                        "A", 1, NA,
                        "A", 2, "x",
                        "B", NA, "y",
                        "C", 3, "z")
 
-  expect_equal( small_tbl |> group_by(group) |>
-                  summarize(across(everything(), ~sum(is.na(.x))),
+  expect_equal( small_tbl |> dplyr::group_by(group) |>
+                  dplyr::summarize(dplyr::across(dplyr::everything(), ~sum(is.na(.x))),
                             .groups = "drop"),
                 count_all_missing_by_group(small_tbl, group)
   )
 
-  expect_equal( small_tbl |> group_by(group) |>
-                  summarize(across(everything(), ~sum(is.na(.x))),
+  expect_equal( small_tbl |> dplyr::group_by(group) |>
+                  dplyr::summarize(dplyr::across(everything(), ~sum(is.na(.x))),
                             .groups = NULL),
                 count_all_missing_by_group(small_tbl, group, NULL)
   )
